6dbbc03d20243dada27314978fe4650a5568a207
Kandadai Madhurima Yasaswini <Madhurima_Kandadai@epam.com>
2016-09-09 10:53:22 +0530

    minot changes

--- ConsoleApp3/src/ConsoleApp3/Entities/Employee.cs
+++ ConsoleApp3/src/ConsoleApp3/Entities/Employee.cs
@@ -1,46 +1,79 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace ConsoleApp3
 {
-    static class SyntaxNodeHelper
-    {
-        public static bool TryGetParentSyntax<T>(SyntaxNode syntaxNode, out T result)
-            where T : SyntaxNode
-        {
-            // set defaults
-            result = null;
-
-            if (syntaxNode == null)
-            {
-                return false;
-            }
-
-            try
-            {
-                syntaxNode = syntaxNode.Parent;
-
-                if (syntaxNode == null)
-                {
-                    return false;
-                }
-
-                if (syntaxNode.GetType() == typeof(T))
-                {
-                    result = syntaxNode as T;
-                    return true;
-                }
-
-                return TryGetParentSyntax<T>(syntaxNode, out result);
-            }
-            catch
-            {
-                return false;
-            }
-        }
-    }
-
     public class Employee
     {
         public int EmpId { get; set; }
+
+        public void MethodFromGoogle()
+        {
+            var list = Directory.EnumerateFiles(Path.GetDirectoryName(@"D:\HyperExamples\ConsoleApp3\src\ConsoleApp3\Entities\"));
+            var exts = Directory.EnumerateFiles(Path.GetDirectoryName(@"D:\HyperExamples\ConsoleApp3\src\ConsoleApp3\Entities\")).Where(x => Path.GetExtension(x) == ".cs");
+            var text = exts.Select(x => CSharpSyntaxTree.ParseText(File.ReadAllText(x))).Cast<CSharpSyntaxTree>();
+            foreach (CSharpSyntaxTree syntaxTree in text)
+            {
+                var root = (CompilationUnitSyntax)syntaxTree.GetRoot();
+                var sd = syntaxTree.GetRoot().DescendantNodes().OfType<ClassDeclarationSyntax>().ToList();
+
+                foreach (var item in sd)
+                {
+                    var fullClassName = item.Identifier.ToString();
+                }
+                var k = sd.Where(x => x.BaseList != null && x.BaseList.Types.Any(y => y.Type is Microsoft.CodeAnalysis.CSharp.Syntax.IdentifierNameSyntax));
+                //        && ( (Microsoft.CodeAnalysis.CSharp.Syntax.IdentifierNameSyntax)y.Type ).Identifier.Text == "Module"));
+                // Get all class declarations in each file that derive from Module
+                foreach (ClassDeclarationSyntax classDeclaration in syntaxTree.GetRoot()
+                    .DescendantNodes()
+                    .OfType<ClassDeclarationSyntax>()
+                    .Where(x => x.BaseList != null && x.BaseList.Types
+                        .Any(y => y.Type is IdentifierNameSyntax
+                            && ( (IdentifierNameSyntax)y.Type ).Identifier.Text == "Module")))
+                {
+                    // Output the same namespace as the class
+                    SyntaxNode namespaceNode = classDeclaration.Parent;
+                    while (namespaceNode != null && !( namespaceNode is NamespaceDeclarationSyntax ))
+                    {
+                        namespaceNode = namespaceNode.Parent;
+                    }
+                    if (namespaceNode != null)
+                    {
+                        Console.WriteLine("namespace " + ( (NamespaceDeclarationSyntax)namespaceNode ).Name.ToString() + Environment.NewLine + "{");
+                    }
+
+                    // Output the extensions class
+                    Console.WriteLine("    public static class " + classDeclaration.Identifier.Text + "PipelineExtensions" + Environment.NewLine + "    {");
+
+                    // Get all non-static public constructors
+                    foreach (ConstructorDeclarationSyntax constructor in classDeclaration.Members
+                        .OfType<ConstructorDeclarationSyntax>()
+                        .Where(x => x.Modifiers.Count == 1 && x.Modifiers[0].Text == "public"))
+                    {
+                        // Output the static constructor method
+                        Console.WriteLine("        public static IPipeline " + classDeclaration.Identifier.Text + constructor.ParameterList.ToString().Insert(1, "this IPipeline pipeline, ") + Environment.NewLine + "        {");
+
+                        // Create and add the module
+                        Console.WriteLine("            return pipeline.AddModule(new " + classDeclaration.Identifier.Text + "(" + string.Join(", ", constructor.ParameterList.Parameters.Select(x => x.Identifier.Text)) + "));");
+
+                        // Close method
+                        Console.WriteLine("        }");
+                    }
+
+                    // Close extensions class
+                    Console.WriteLine("    }");
+
+                    // Close namespace
+                    if (namespaceNode != null)
+                    {
+                        Console.WriteLine("}");
+                    }
+                }
+            }
+        }
     }
 }
--- ConsoleApp3/src/ConsoleApp3/Program.cs
+++ ConsoleApp3/src/ConsoleApp3/Program.cs
@@ -13,76 +13,39 @@ namespace ConsoleApp3
     {
         public static void Main(string[] args)
         {
-            var list = Directory.EnumerateFiles(Path.GetDirectoryName(@"D:\HyperExamples\ConsoleApp3\src\ConsoleApp3\Entities\"));
-            var exts = Directory.EnumerateFiles(Path.GetDirectoryName(@"D:\HyperExamples\ConsoleApp3\src\ConsoleApp3\Entities\")).Where(x => Path.GetExtension(x) == ".cs");
-            var text = exts.Select(x => CSharpSyntaxTree.ParseText(File.ReadAllText(x))).Cast<CSharpSyntaxTree>();            
+            List<string> classNames = new List<string>();
+            var list = Directory.EnumerateFiles(Path.GetDirectoryName(@"Entities\")).Where(x => Path.GetExtension(x) == ".cs");
+            var text = list.Select(x => CSharpSyntaxTree.ParseText(File.ReadAllText(x))).Cast<CSharpSyntaxTree>();
             foreach (CSharpSyntaxTree syntaxTree in text)
             {
                 var root = (CompilationUnitSyntax)syntaxTree.GetRoot();
-
-                var classes = root.DescendantNodes().OfType<ClassDeclarationSyntax>();
-                var dd = syntaxTree.GetCompilationUnitRoot().ChildNodes().ToList();
                 var sd = syntaxTree.GetRoot().DescendantNodes().OfType<ClassDeclarationSyntax>().ToList();
                 foreach (var item in sd)
                 {
-                    NamespaceDeclarationSyntax namespaceDeclarationSyntax = null;
-                    if (!SyntaxNodeHelper.TryGetParentSyntax(item, out namespaceDeclarationSyntax))
-                    {
-                        return; // or whatever you want to do in this scenario
-                    }
-
-                    var namespaceName = namespaceDeclarationSyntax.Name.ToString();
-                    var fullClassName = item.Identifier.ToString();
-                }
-                var k = sd.Where(x => x.BaseList != null && x.BaseList.Types.Any(y => y.Type is Microsoft.CodeAnalysis.CSharp.Syntax.IdentifierNameSyntax));
-                //        && ( (Microsoft.CodeAnalysis.CSharp.Syntax.IdentifierNameSyntax)y.Type ).Identifier.Text == "Module"));
-                // Get all class declarations in each file that derive from Module
-                foreach (ClassDeclarationSyntax classDeclaration in syntaxTree.GetRoot()
-                    .DescendantNodes()
-                    .OfType<ClassDeclarationSyntax>()
-                    .Where(x => x.BaseList != null && x.BaseList.Types
-                        .Any(y => y.Type is Microsoft.CodeAnalysis.CSharp.Syntax.IdentifierNameSyntax
-                            && ( (Microsoft.CodeAnalysis.CSharp.Syntax.IdentifierNameSyntax)y.Type ).Identifier.Text == "Module")))
-                {
-                    // Output the same namespace as the class
-                    SyntaxNode namespaceNode = classDeclaration.Parent;
-                    while (namespaceNode != null && !( namespaceNode is NamespaceDeclarationSyntax ))
-                    {
-                        namespaceNode = namespaceNode.Parent;
-                    }
-                    if (namespaceNode != null)
-                    {
-                        Console.WriteLine("namespace " + ( (NamespaceDeclarationSyntax)namespaceNode ).Name.ToString() + Environment.NewLine + "{");
-                    }
-
-                    // Output the extensions class
-                    Console.WriteLine("    public static class " + classDeclaration.Identifier.Text + "PipelineExtensions" + Environment.NewLine + "    {");
-
-                    // Get all non-static public constructors
-                    foreach (ConstructorDeclarationSyntax constructor in classDeclaration.Members
-                        .OfType<ConstructorDeclarationSyntax>()
-                        .Where(x => x.Modifiers.Count == 1 && x.Modifiers[0].Text == "public"))
-                    {
-                        // Output the static constructor method
-                        Console.WriteLine("        public static IPipeline " + classDeclaration.Identifier.Text + constructor.ParameterList.ToString().Insert(1, "this IPipeline pipeline, ") + Environment.NewLine + "        {");
-
-                        // Create and add the module
-                        Console.WriteLine("            return pipeline.AddModule(new " + classDeclaration.Identifier.Text + "(" + string.Join(", ", constructor.ParameterList.Parameters.Select(x => x.Identifier.Text)) + "));");
-
-                        // Close method
-                        Console.WriteLine("        }");
-                    }
-
-                    // Close extensions class
-                    Console.WriteLine("    }");
-
-                    // Close namespace
-                    if (namespaceNode != null)
-                    {
-                        Console.WriteLine("}");
-                    }
+                    classNames.Add(item.Identifier.ToString());
                 }
             }
+
+            foreach (var item in classNames)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+
+        public static void CreateClass(string ClassName)
+        {
+            //SF Syntax Factory
+            //SyntaxFactory SF;
+            //CompilationUnitSyntax cu = SF.CompilationUnit();
+//            CompilationUnitSyntax cu = SF.CompilationUnit()
+//    .AddUsings(SF.UsingDirective(SF.IdentifierName("System")))
+//    .AddUsings(SF.UsingDirective(SF.IdentifierName("System.Generic")))
+//    ;
+//            cu.AddUsings(SF.UsingDirective(SF.IdentifierName("System")));
+//cu = cu.AddUsings(SF.UsingDirective(SF.IdentifierName("System")));
+//                NamespaceDeclarationSyntax ns = SF.NamespaceDeclaration(SF.IdentifierName("MyNamespace"));
+//            cu = cu.AddMembers(ns);
         }
     }
 }
